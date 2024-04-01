2191f6fa5385a3bcee9397e369d7cf727d2da447
aditikagalkar <78659700+aditikagalkar@users.noreply.github.com>
2024-04-01 21:17:57 +0000

    Header from Scratch

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,15 @@ import CalendarComponent from './components/CalendarComponent'
 function App() {
   return (
     <div className="App">
-     <header>
-     {/* from: https://www.sliderrevolution.com/resources/css-header/ */}
-     <div class="overlay">
-     <h1>The best planner ever</h1>
-     <h3>showing the other font!</h3>
-      </div>
-     </header>
-    
-        <CalendarComponent />
+      <header class="header">
+        <div >
+          <h1>Planner</h1>
+        </div>
+      </header>
+      <CalendarComponent />
     </div>
 
-  )}
+  )
+}
 
-  export default App;
+export default App;
