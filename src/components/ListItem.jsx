31f886e60df06be015bff4f14381b6f230f50bd0
aditikagalkar <78659700+aditikagalkar@users.noreply.github.com>
2024-04-09 23:06:37 +0000

    Remove task needs to be fixed.

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
 import "../styles/ListItem.css"
+
 const ListItem = (props) => {
+
+    const handleCheck = () => {
+        props.remove(props.task);
+    }
+
     return (
         <div>
             <li>
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                    <label class="form-check-label" for="flexCheckDefault">
-                        {props.description}
-                    </label>
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={handleCheck} />
+                <label class="form-check-label" for="flexCheckDefault">
+                    {props.description}
+                </label>
             </li>
         </div>
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -15,7 +15,17 @@ const TodoList = () => {
 
     const handleTask = (e) => {
         setTask(e.target.value);
-      };
+    };
+
+    // NEEDS WORK
+    // is clearing entire list
+    const handleRemove = (taskToRemove) => {
+        setTodoItems((previousValues) => {
+            const updatedTodoItems = previousValues.filter(item => item.task !== taskToRemove);
+            localStorage.setItem('my-todo-items', JSON.stringify(updatedTodoItems));
+            return updatedTodoItems;
+        })
+    }
 
     //Add todo item to list and update local storage list
     const addItem = () => {
@@ -31,21 +41,21 @@ const TodoList = () => {
             setTask('');
         }
     }
-    
+
 
     return (
         <>
             <h2>Todo List</h2>
             <div>
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Add new task" aria-label="Add new task" aria-describedby="button-addon2" value={task} onChange={handleTask}/>
+                    <input type="text" class="form-control" placeholder="Add new task" aria-label="Add new task" aria-describedby="button-addon2" value={task} onChange={handleTask} />
                     <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={addItem}><IoMdAdd />
                     </button>
                 </div>
                 <div class='list-wrapper'>
                     <ul class='todo-list'>
                         {/* map of list items goes here */
-                        todoItems.map((item, i)=><ListItem key={i} description={item.description}/>)
+                            todoItems.map((item, i) => <ListItem key={i} description={item.description} remove={handleRemove}/>)
                         }
                     </ul>
                 </div>
