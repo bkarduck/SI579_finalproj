822816231bebd355a42e7bbbf393d934baf85741
aditikagalkar <78659700+aditikagalkar@users.noreply.github.com>
2024-04-12 04:37:39 +0000

    clear comments

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -10,29 +10,14 @@ const ListItem = (props) => {
 
     return (
         <div>
-            {/* <li>
-                <input class="form-check-input" type="button" value="" id="flexCheckDefault" onChange={() => handleCheck} />
-                <label class="form-check-label" for="flexCheckDefault">
-                    {props.description}
-                </label>
-            </li> */}
-
-
-            <li> 
-            {/* outline-dark
-            variant="primary" className="w-100 fs-5 mb-2"
-            className='btn btn-sm btn-danger done' */}
-            <button variant="outline-dark" type="button" onClick={() => props.remove(props.description)}>
-                     ☑️
-                    </button>
-            <span className='px-2'> {props.description}</span>
-           
-          
-                    
+            <li>
+                <button variant="outline-dark" type="button" onClick={() => props.remove(props.description)}>
+                    ☑️
+                </button>
+                <span className='px-2'> {props.description}</span>
             </li>
-       
         </div>
-    )
+    );
 }
 
 export default ListItem;
