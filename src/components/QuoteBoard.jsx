b1323f2a7b22276f7dcfbf727a7c64c0d29ae7b0
aditikagalkar <78659700+aditikagalkar@users.noreply.github.com>
2024-04-12 15:53:48 +0000

    Connect Quote API

--- src/components/QuoteBoard.jsx
+++ src/components/QuoteBoard.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 const QuoteBoard = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.api-ninjas.com/v1/quotes?category=inspirational', {
+            headers: {
+                'X-Api-Key': 'jD+C9ps5WZ2Z3KvPlJW+Ew==Yav7ceUyxVveNdJZ'
+            }
+        })
+            .then(response => response.json())
+            .then(json => setData(json[0])) // Assuming the API always returns a single quote
+            .catch(error => console.error(error));
+    }, []);
+    console.log(data);
+
     return (
         <>
-            <h2>Quote Board</h2>
+            <div>
+                <h2>Quote Board</h2>
+                {data && (
+                    <div>
+                        <p>"{data.quote}"</p>
+                        <p>- {data.author}</p>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
