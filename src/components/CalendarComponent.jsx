82ce28c2a32e03d18b0fc8ba993efe2d12c9e57d
Bella Karduck <bkarduck@umich.edu>
2024-04-19 00:46:37 +0000

    yayy calendar progress

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -3,28 +3,83 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import {useState} from "react";
+import DialogComponent from './DialogComponent'
 
+// need to run: 
+// npm install @mui/material @emotion/react @emotion/styled
 
 const localizer = momentLocalizer(moment);
 
-const CalendarComponent = (props) => {
-    const [view, setView] = useState('month');
+const CalendarComponent = () => {
+
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [events, setEvents] = useState(() => {
+    const storedEvents = localStorage.getItem('my-events');
+    if (storedEvents) {
+        return JSON.parse(storedEvents)
+    }
+    return [];
+    });
+
+  
+  const [open, setOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState();
+
+    const handleSelectSlot = ( event) => {
+      const start = event.start
+      const end = event.end
+    if (!end) {
+      setSelectedDate({ start, start });
+    }
+    else {
+      setSelectedDate({ start, end });
+    }
+
+    const date = new Date();
+    setOpen(true);
+  };
+
+  const handleSelectEvent = (event) => {
+
+    setSelectedEvent(event)
+    setOpen(true);
+  };
+
+
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedEvent(null);
+
+  };
+  
+    // const [view, setView] = useState('month');
     return (
         <div>
-          <select value={view} onChange={(e) => setView(e.target.value)}>
+          {/* <select value={view} onChange={(e) => setView(e.target.value)}>
             <option value="week">Week</option>
             <option value="month">Month</option>
             <option value="day">Day</option>
-          </select>
+          </select> */}
           <div style={{ height: 400 }}>
             <Calendar
               localizer={localizer}
-              // events={[]}
+              events={events}
+              selectable
               startAccessor="start"
               endAccessor="end"
-              view={view}
-              onView={(view) => setView(view)}
+              views={['week', 'month', 'day', 'agenda']}  // Specify available views
+
+              // view={view}
+              // onView={(view) => 
+              //   setView(view)}
+              onSelectEvent={handleSelectEvent}
+              onSelectSlot={handleSelectSlot}
+              
             />
+          <DialogComponent open={open} onClose={handleClose} selectedDate={selectedDate} setEvents={setEvents} selectedEvent={selectedEvent}  />
+
+
           </div>
         </div>
       );
---
+++ src/components/DialogComponent.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
+
+const DialogComponent = ({ open, onClose, selectedDate, setEvents, selectedEvent }) => {
+
+  const [title, setTitle] = useState('');
+
+
+
+
+const handleSaveEvent = () => {
+
+    if (selectedEvent) {
+        setEvents((prevEvents) =>
+                prevEvents.map((event) => (event.id === selectedEvent.id ? { ...event, title } : event))
+              );
+
+            } else {
+    setEvents(prevEvents => {
+        const updatedEvents = [...prevEvents, {
+            title,
+            start: selectedDate.start,
+            end: selectedDate.end,
+            id: new Date().getTime()
+          }];
+  
+      localStorage.setItem('my-events', JSON.stringify(updatedEvents)); // Update local storage here
+      return updatedEvents;
+    });}
+    setTitle('');
+    onClose();
+  };
+
+
+  
+  const handleDeleteEvent = () => {
+  
+    setEvents(prevEvents => {
+      const filteredEvents = prevEvents.filter(event => event.id !== selectedEvent.id);
+      localStorage.setItem('my-events', JSON.stringify(filteredEvents)); // Update local storage here
+      return filteredEvents;
+    });
+    onClose();
+  };
+
+
+  
+
+  
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{selectedEvent ? "Edit Event" : "Add New Event"}</DialogTitle>
+
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Event Title"
+          type="text"
+          fullWidth
+          variant="standard"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={() => handleSaveEvent()}>Save</Button>
+        {selectedEvent && <Button onClick={() => handleDeleteEvent()} color="secondary">Delete</Button>}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default DialogComponent;
